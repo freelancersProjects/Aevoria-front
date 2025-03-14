21fc26f78b506c6871a3c2a3352794effedad03d
Mathis Bls <mathis.oxym@gmail.com>
2025-03-14 19:39:10 +0100

    hide layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header/Header'
-import Login from './components/Login/login'
-import './App.css'
-import Footer from './components/Footer/Footer'
-import { LanguageProvider } from './translations/LanguageContext'
-import Home from './pages/Home/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { LanguageProvider } from './translations/LanguageContext';
+import Home from './pages/Home/Home';
+import Login from './components/Login/login';
+import './App.css';
+
+const hiddenLayouts = ['/login', '/register'];
+
+function Layout({ children }) {
+  const location = useLocation();
+  const hideLayout = hiddenLayouts.includes(location.pathname);
+
+  return (
+    <div className="app-container">
+      {!hideLayout && <Header />}
+      <div className="content-container">{children}</div>
+      {!hideLayout && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <LanguageProvider>
-      <div id="root">
-        <Router>
-          <div className="app-container">
-            <Header />
-            <div className="content-container">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-              </Routes>
-              <Footer />
-            </div>
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Layout>
+      </Router>
     </LanguageProvider>
-  )
+  );
 }
 
-export default App 
+export default App;
