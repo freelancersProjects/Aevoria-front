1d788e444302f4b6beaab6ade6ff4bca34e6b1d4
Mathis Bls <mathis.oxym@gmail.com>
2025-03-17 00:21:09 +0100

    useAuth change routing

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,18 +1,22 @@
 import { useState, useEffect } from "react";
 import apiService from "../services/apiService";
+import jwtDecode from "jwt-decode";
 
 const useAuth = () => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    // On vérifie si l'utilisateur est déjà connecté
     useEffect(() => {
         const fetchUser = async () => {
             try {
                 const token = localStorage.getItem("token");
                 if (token) {
                     apiService.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-                    const response = await apiService.get("/auth/me");
+
+                    const decodedToken = jwtDecode(token);
+                    const userId = decodedToken.id;
+
+                    const response = await apiService.get(`/api/users/${userId}`);
                     setUser(response.data);
                 }
             } catch (error) {
