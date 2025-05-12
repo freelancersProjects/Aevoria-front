4271dcf0d3f22f63c6ee4db58f00813c07800222
MathisBls <mathis.oxym@gmail.com>
2025-05-12 20:03:15 +0200

    patch comit Luca

--- src/components/AEV/AEV.Accordion/Accordion.jsx
+++ src/components/AEV/AEV.Accordion/Accordion.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import ArrowDown from "../../../../public/assets/svg/arrow-down.svg";
-import ArrowDown from "../../../assets/svg/arrow-down.svg";
 
 
 import "./Accordion.scss";
